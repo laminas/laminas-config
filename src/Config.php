351a793f20cd928483489f31e09956f07facfc8a
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:29:29 -0600

    Rewriting as Laminas Project package

--- src/Config.php
+++ src/Config.php
@@ -1,13 +1,12 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-config for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-config/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-config/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Config;
+namespace Laminas\Config;
 
 use ArrayAccess;
 use Countable;
@@ -134,7 +133,7 @@ class Config implements Countable, Iterator, ArrayAccess
     }
 
     /**
-     * Deep clone of this instance to ensure that nested Zend\Configs are also
+     * Deep clone of this instance to ensure that nested Laminas\Configs are also
      * cloned.
      *
      * @return void
