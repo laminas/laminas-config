71a31cb421c37665e47732959ceb9d4d4b6e2e65
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:29:17 -0600

    Rewriting as Laminas Project package

--- src/ReaderPluginManager.php
+++ src/ReaderPluginManager.php
@@ -1,15 +1,14 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-config for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-config/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-config/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Config;
+namespace Laminas\Config;
 
-use Zend\ServiceManager\AbstractPluginManager;
+use Laminas\ServiceManager\AbstractPluginManager;
 
 class ReaderPluginManager extends AbstractPluginManager
 {
@@ -19,10 +18,10 @@ class ReaderPluginManager extends AbstractPluginManager
      * @var array
      */
     protected $invokableClasses = array(
-        'ini'  => 'Zend\Config\Reader\Ini',
-        'json' => 'Zend\Config\Reader\Json',
-        'xml'  => 'Zend\Config\Reader\Xml',
-        'yaml' => 'Zend\Config\Reader\Yaml',
+        'ini'  => 'Laminas\Config\Reader\Ini',
+        'json' => 'Laminas\Config\Reader\Json',
+        'xml'  => 'Laminas\Config\Reader\Xml',
+        'yaml' => 'Laminas\Config\Reader\Yaml',
     );
 
     /**
