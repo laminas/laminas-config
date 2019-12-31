b46f7ba965ed874b655d3519e0a9c1dea08f7bd5
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:29:41 -0600

    Rewriting as Laminas Project package

--- test/Writer/IniTest.php
+++ test/Writer/IniTest.php
@@ -1,20 +1,19 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-config for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-config/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-config/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Config\Writer;
+namespace LaminasTest\Config\Writer;
 
-use Zend\Config\Writer\Ini as IniWriter;
-use Zend\Config\Config;
-use Zend\Config\Reader\Ini as IniReader;
+use Laminas\Config\Config;
+use Laminas\Config\Reader\Ini as IniReader;
+use Laminas\Config\Writer\Ini as IniWriter;
 
 /**
- * @group      Zend_Config
+ * @group      Laminas_Config
  */
 class IniTest extends AbstractWriterTestCase
 {
