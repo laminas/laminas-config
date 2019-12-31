5a8f5694456ef0e814a4a82478af444524f86687
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:29:55 -0600

    Rewriting as Laminas Project package

--- src/AbstractConfigFactory.php
+++ src/AbstractConfigFactory.php
@@ -1,16 +1,15 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-config for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-config/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-config/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Config;
+namespace Laminas\Config;
 
+use Laminas\ServiceManager;
 use Traversable;
-use Zend\ServiceManager;
 
 /**
  * Class AbstractConfigFactory
