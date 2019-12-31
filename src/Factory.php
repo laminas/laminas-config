a5856e32b369d9f2f3f98ab36d7544de75048008
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:29:23 -0600

    Rewriting as Laminas Project package

--- src/Factory.php
+++ src/Factory.php
@@ -1,15 +1,14 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-config for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-config/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-config/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Config;
+namespace Laminas\Config;
 
-use Zend\Stdlib\ArrayUtils;
+use Laminas\Stdlib\ArrayUtils;
 
 class Factory
 {
@@ -138,7 +137,7 @@ class Factory
             (!is_object($config) && !is_array($config))
         ) {
             throw new Exception\InvalidArgumentException(
-                __METHOD__." \$config should be an array or instance of Zend\\Config\\Config"
+                __METHOD__." \$config should be an array or instance of Laminas\\Config\\Config"
             );
         }
 
